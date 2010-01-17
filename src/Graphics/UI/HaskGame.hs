665b7440deb52b48f55f304e5e0f0be4d594ea3e
Eyal Lotem <eyal.lotem@gmail.com>
2010-01-18 01:58:33 +0200

    Check sdl returned bools. Avoid warnings

--- src/Graphics/UI/HaskGame.hs
+++ src/Graphics/UI/HaskGame.hs
@@ -35,23 +35,36 @@ createRGBSurface (Vector2 w h) = do
      w h 32 0xFF 0xFF00 0xFF0000 0x00000000
   SDL.displayFormatAlpha surface
 
+sdlCall :: String -> IO Bool -> IO ()
+sdlCall name act = do
+  res <- act
+  if not res
+    then
+      fail ("SDL " ++ name ++ " failed")
+    else
+      return ()
+
+sdlBlitSurface :: SDL.Surface
+               -> Maybe Rect
+               -> SDL.Surface
+               -> Maybe Rect
+               -> IO ()
+sdlBlitSurface src srcRect dest destRect =
+    sdlCall "blitSurface" $ SDL.blitSurface src srcRect dest destRect
+
 blit :: Surface -> Vector2 Int -> Surface -> IO ()
-blit dest pos src = do
-  SDL.blitSurface src Nothing dest (Just . Rect.makeFromPos $ pos)
-  return ()
+blit dest pos src = sdlBlitSurface src Nothing dest (Just . Rect.makeFromPos $ pos)
 
 blitPart :: Surface -> Vector2 Int -> Surface -> Rect -> IO ()
-blitPart dest destPos src srcRect = do
-  SDL.blitSurface
-     src  (Just . Rect.trunc $ srcRect)
-     dest (Just . Rect.makeFromPos $ destPos)
-  return ()
+blitPart dest destPos src srcRect =
+    sdlBlitSurface
+        src  (Just . Rect.trunc $ srcRect)
+        dest (Just . Rect.makeFromPos $ destPos)
 
 sdlFillRect :: Surface -> Maybe Rect -> Color -> IO ()
 sdlFillRect surface mRect color = do
   fillerPixel <- pixel surface color
-  SDL.fillRect surface mRect fillerPixel
-  return ()
+  sdlCall "fillRect" $ SDL.fillRect surface mRect fillerPixel
 
 fillSurface :: Surface -> Color -> IO ()
 fillSurface surface color = sdlFillRect surface Nothing color
